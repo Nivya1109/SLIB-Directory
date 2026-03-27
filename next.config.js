14cf6587de2e60da8d1193b12725da16ec2a3965
Nivya <npoojar1@umbc.edu>
2026-03-27 01:03:44 -0400

    fix: transpile react-markdown for Next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['react-markdown'],
   eslint: {
     // Disable ESLint during production builds
     ignoreDuringBuilds: true,
